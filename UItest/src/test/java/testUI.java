502b2833d301d3b82112271b9e7b70b049fc1fab
Ekaterina Pinchuk <pinchuk.ekatya@gmail.com>
2023-05-11 00:06:56 +0600

    UItest

--- UItest/src/test/java/testUI.java
+++ UItest/src/test/java/testUI.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class testUI {
-}
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import java.util.List;
+
+public class testUI {
+
+        private WebDriver driver;
+        JavascriptExecutor js;
+
+        @Before
+        public void setUp() {
+
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Katya\\OneDrive\\Рабочий стол\\chromedriver_win32 (1)\\chromedriver.exe");
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver(options);
+            js = (JavascriptExecutor) driver;
+
+        }
+
+        @After
+        public void tearDown() {
+            driver.quit();
+        }
+
+        @Test
+        public void pageTest() {
+            driver.get("https://playground.learnqa.ru/puzzle/triangle");
+            List<WebElement> elements1 = driver.findElements(By.id("show_answ"));
+            assert(elements1.size() > 0);
+            driver.findElement(By.id("show_answ")).click();
+            List<WebElement> elements2 = driver.findElements(By.linkText("Ссылка на ответы"));
+            assert(elements2.size() > 0);
+            List<WebElement> elements3 = driver.findElements(By.id("hide_answ"));
+            assert(elements3.size() > 0);
+        }
+    }
+
